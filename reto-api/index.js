50c2db7392c7cde6210f3254c2df7408a663a117
stgosm <santiago.serrano.marquez@hotmail.com>
2020-09-28 21:52:09 -0500

    Resolviendo reto

--- reto-api/index.js
+++ reto-api/index.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 axios.get('https://swapi.dev/api/people')
   .then(response => {
@@ -7,4 +9,34 @@ axios.get('https://swapi.dev/api/people')
     const females = personajes.filter(personaje => personaje.gender === 'female');
     // Generate female.json y male.json
     // Crear el archivo
+
+    const fileFemale = path.join(__dirname, 'female.json');
+    const fileMale = path.join(__dirname, 'male.json');
+
+    const crearArchivo = (pathName) => {
+    fs.writeFile(pathName, '', (err) => {
+        // if (err) throw err;
+        console.log('El archivo ha sido creado');
+    });
+    }
+
+    const crearPersonaje = (pathName, personajes) => {
+        fs.appendFileSync(pathName, `${personajes}`, { encoding: 'utf-8' });
+    }
+
+    const verificarArchivo = (pathName, data) => {
+        fs.stat(pathName, (err, stats) => {
+            if (err) {
+                crearArchivo(pathName);
+            } else {
+              console.log('El archivo ya existe');
+            };
+    
+            crearPersonaje(pathName, JSON.stringify(data));
+        });
+    }    
+
+    verificarArchivo(fileFemale, females);
+    verificarArchivo(fileMale, males);
+
   });
